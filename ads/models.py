f9fb1e3714e6005e61e9a27c3de5e7e2e37d1fdd
ripudaman <ripudaman@asynchain.com>
2023-05-26 17:41:55 +0530

    msg

--- ads/models.py
+++ ads/models.py
@@ -21,7 +21,7 @@ PROFILE_STATUS = (
 )
 
 class Agency(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100, null=True, blank=True)
     phone = models.CharField(max_length=15, null=True, blank=True)
     whatsapp = models.CharField(max_length=15, null=True, blank=True)
