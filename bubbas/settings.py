81d624ffefb84b0e86565dfd274e237db0d6a4cc
ripudaman <ripudaman@asynchain.com>
2023-06-02 14:32:00 +0530

    msg

--- bubbas/settings.py
+++ bubbas/settings.py
@@ -179,7 +179,7 @@ STATICFILES_FINDERS = (
 CACHES = {
     'default':{
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'dataflai_cache',
+        'LOCATION': 'gaurav_cache',
         'TIMEOUT': 300,
     }
 }
