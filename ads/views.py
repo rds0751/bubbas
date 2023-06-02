6397760a0c8d7cde25600fcef12f82e98546157d
ripudaman <ripudaman@asynchain.com>
2023-06-02 14:02:10 +0530

    msg

--- ads/views.py
+++ ads/views.py
@@ -7,7 +7,7 @@ from home.models import Data
 # Create your views here.
 def home(request):
     c = City.objects.all().order_by('?')
-    ad_count = Ad.objects.all().count()
+    ad_count = Ad.objects.all()
     meta_title = Data.objects.get(key='home_meta_title').value.replace('%ad_count', str(ad_count))
     meta_description = Data.objects.get(key='home_meta_description').value
     site_name = Data.objects.get(key='site_name').value
