72ce77cefdbb7bc2034de7f24d6427102c2f9908
ripudaman <ripudaman@asynchain.com>
2023-06-02 13:16:24 +0530

    msg

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Data(models.Model):
     key    = models.CharField(max_length=100, null=True, blank=True)
-    value  = models.TextField(null=True, blank=True)
+    value  = RichTextField(null=True, blank=True)
     status = models.CharField(max_length=25, default="Enabled", null=True, blank=True)
     image  = models.FileField(null=True, blank=True)
 
