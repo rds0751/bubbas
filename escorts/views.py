55262d4353c2bdbb7f68a20eb7cfa161b6570d45
ripudaman <ripudaman@asynchain.com>
2023-06-04 16:02:02 +0530

    msg

--- escorts/views.py
+++ escorts/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
-from ads.models import Image, Ad, City, Category
+from ads.models import Ad, City, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from home.models import Data
 
 # Create your views here.
 def city(request, city):
-    city = City.objects.get(slug__icontains=city)
+    city = City.objects.get(slug=city)
     posts = Ad.objects.filter(city=city, profile_status='Escorts').order_by('?')
     c = City.objects.all().order_by('?')
     ca = Category.objects.all().order_by('?')[:50]
     p = Ad.objects.all().order_by('?')[:30]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts, 5)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     if city.meta_title:
         meta_title = city.meta_title
     else:
-        meta_title = str(city.get_no_of_es_ads) + ' ' + city.name + ' Escorts & ' + str(int(city.get_no_of_es_ads*2.20234)) + ' Photos with Whatsapp Number'
+        meta_title = str(city.get_no_of_es_ads) + ' ' + str(city.name) + ' Escorts & ' + str(int(city.get_no_of_es_ads*2.30865)) + ' Photos with Whatsapp Number'
     if city.meta_description:
         meta_description = city.meta_description
     else:
-        meta_description = 'Find ' + city.name + ' Escorts with Real Photos and Number Incall & Outcall pick and drop service at Bubbas has all the latest ads on '+ + city.name + +', updated every day.'
+        meta_description = 'Find ' + str(city.name) + ' Escorts with Real Photos and Number Incall & Outcall pick and drop service at Bubbas has all the latest ads on '+ str(city.name) +', updated every day.'
     context = {
         'cities': c,
         'categories': ca,
@@ -32,7 +41,7 @@ def city(request, city):
 
 # Create your views here.
 def ad(request, slug, city, id):
-    city = City.objects.get(slug=city)
+    city = City.objects.get(slug__icontains=city)
     posts = Ad.objects.filter(city=city, profile_status='Escorts').order_by('?')[:4]
     posts1 = Ad.objects.filter(city=city, profile_status='Escorts').order_by('?')[:4]
     posts2 = Ad.objects.filter(city=city, profile_status='Escorts').order_by('?')[:4]
@@ -44,5 +53,6 @@ def ad(request, slug, city, id):
         'posts': posts,
         'meta_title': meta_title,
         'meta_description': meta_description
+
     }
     return render(request, 'ad.html', context)
