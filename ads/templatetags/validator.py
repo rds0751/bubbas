e40a131aed90e688e4bf0fb1c236122944773625
ripudaman <ripudaman@asynchain.com>
2023-06-05 15:13:31 +0530

    msg

--- ads/templatetags/validator.py
+++ ads/templatetags/validator.py
@@ -7,9 +7,4 @@ register = template.Library()
 
 @register.filter(name="validate")
 def validate(value):
-    try:
-        print(default_storage.exists(str(settings.BASE_DIR) + value))
-    except Exception as e:
-        print(e)
-        return False
-    return True
+    return default_storage.exists(str(settings.BASE_DIR) + value)
