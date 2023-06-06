0f59badc74b90d9f1f7d9d54ed92cc5e1b2b3a31
ripudaman <ripudaman@asynchain.com>
2023-06-06 16:20:21 +0530

    msg

--- bubbas/settings.py
+++ bubbas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%=*+3ohnfapm8mz)ma-gt(xzckuawdof%7w5i9gcp!$v5ctm*z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
