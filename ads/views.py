3e11ab410837ca195e1c7b351eba12b4a9800e68
ripudaman <ripudaman@asynchain.com>
2023-06-07 18:07:25 +0530

    msg

--- ads/views.py
+++ ads/views.py
@@ -39,6 +39,37 @@ class AdDeleteView(LoginRequiredMixin, DeleteView):
 
 # Create your views here.
 def home(request):
+    c = City.objects.filter(parent_city=None).order_by('?')
+    cg_ad_count = Ad.objects.filter(profile_status="Call Girls").count()
+    es_ad_count = Ad.objects.filter(profile_status="Escorts").count()
+    meta_title = Data.objects.get(key='home_meta_title').value.replace('%cg_ad_count', str(cg_ad_count)).replace('<p>', '').replace('</p>', '').replace('%es_ad_count', str(es_ad_count))
+    meta_description = Data.objects.get(key='home_meta_description').value
+    site_name = Data.objects.get(key='site_name').value
+    site_logo = Data.objects.get(key='site_logo')
+    page_content = Data.objects.get(key='home_content').value
+    context = {
+        'cities': c,
+        'meta_title': meta_title, 
+        'meta_description': meta_description,
+        'site_logo': site_logo,
+        'site_name': site_name,
+        'page_content': page_content
+    }
+    if request.POST:
+        url = request.POST.get('url')
+        request.session["accepted_cookies"] = True
+        print(request.session["accepted_cookies"])
+        return redirect(url)
+    else:
+        try:
+            print(request.session["accepted_cookies"])
+        except Exception as e:
+            request.session["accepted_cookies"] = False
+        return render(request, 'home.html', context)
+
+
+# Create your views here.
+def all(request):
     c = City.objects.all().order_by('?')
     ad_count = Ad.objects.all().count()
     meta_title = Data.objects.get(key='home_meta_title').value.replace('%ad_count', str(ad_count)).replace('<p>', '').replace('</p>', '')
