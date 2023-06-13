ccc75f4eba17807fa1e29e45b8c7e767f6419611
ripudaman <ripudaman@asynchain.com>
2023-06-13 09:43:05 +0530

    msg

--- ads/management/commands/load_initial_data.py
+++ ads/management/commands/load_initial_data.py
@@ -32,21 +32,3 @@ class Command(BaseCommand):
             i.thumbnail.save(rn, file, save=True)
             i.save()
             print(i.id)
-            image_temp_file = NamedTemporaryFile(delete=True)
-            in_memory_image = open('/home/ubuntu/django/bubbas/medias/'+rn, 'rb')
-            print(in_memory_image)
-            for block in in_memory_image.read(1024 * 8):
-                    print('1')
-                    if not block:
-                            print('2')
-                            break
-                    print('3')
-                    image_temp_file.write(block)
-                    print('4')
-            file_name = rn
-            image_temp_file.flush()
-            temp_file = files.File(image_temp_file, name=file_name)
-            i.thumbnail = temp_file
-            i.thumbnail.save(rn, in_memory_image, save=True)
-            i.save()
-            print(i.id)
